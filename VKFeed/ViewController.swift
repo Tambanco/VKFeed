7de6ba2e4735617ce431d91217ef0e0f23821d41
Al <alexunder_5542@mail.ru>
2021-06-15 11:02:38 +0300

    Initial Commit

---
+++ VKFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKFeed
+//
+//  Created by tambanco 🥳 on 15.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
