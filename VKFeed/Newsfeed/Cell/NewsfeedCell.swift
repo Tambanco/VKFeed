9953f88a04b51b8bbd68b5aee210d4a287c9868c
Al <alexunder_5542@mail.ru>
2021-06-16 14:28:43 +0300

    added outlets to NewsfeedCell class

--- VKFeed/Newsfeed/Cell/NewsfeedCell.swift
+++ VKFeed/Newsfeed/Cell/NewsfeedCell.swift
@@ -12,6 +12,17 @@ class NewsfeedCell: UITableViewCell {
     
     static let reuseId = "NewsfeedCell"
     
+    // MARK: - Outlets
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var postLabel: UILabel!
+    @IBOutlet weak var LikesLabel: UILabel!
+    @IBOutlet weak var commentsLabel: UILabel!
+    @IBOutlet weak var sharesLabel: UILabel!
+    @IBOutlet weak var viewsLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
