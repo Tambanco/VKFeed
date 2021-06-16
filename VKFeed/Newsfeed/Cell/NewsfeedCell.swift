5f2611a70623cff599f8c39189a77cbe89a395ae
Al <alexunder_5542@mail.ru>
2021-06-16 13:41:48 +0300

    added NewsfeedCell with xib

---
+++ VKFeed/Newsfeed/Cell/NewsfeedCell.swift
@@ -0,0 +1,16 @@
+//
+//  NewsfeedCell.swift
+//  VKFeed
+//
+//  Created by tambanco 🥳 on 16.06.2021.
+//
+
+import Foundation
+import UIKit
+
+class NewsfeedCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
