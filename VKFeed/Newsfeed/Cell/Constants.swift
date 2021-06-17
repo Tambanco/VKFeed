047e857c051ccd17460b1bcc486cfff6b2560409
Al <alexunder_5542@mail.ru>
2021-06-17 10:48:28 +0300

    minor refactoring

---
+++ VKFeed/Newsfeed/Cell/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  VKFeed
+//
+//  Created by tambanco 🥳 on 17.06.2021.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    static let cardInsets = UIEdgeInsets(top: 0, left: 8, bottom: 12, right: 8)
+    static let topViewHeight: CGFloat = 36
+    static let postLabelInsets = UIEdgeInsets(top: 8 + Constants.topViewHeight + 8, left: 8, bottom: 8, right: 8)
+    static let postLabelFont = UIFont.systemFont(ofSize: 15)
+    static let bottomViewHeight: CGFloat = 50
+}
--- VKFeed/Newsfeed/Cell/NewsfeedCodeCell.swift
+++ VKFeed/Newsfeed/Cell/NewsfeedCodeCell.swift
@@ -15,13 +15,36 @@ final class NewsfeedCodeCell: UITableViewCell {
     let cardView: UIView = {
         let view = UIView()
         view.backgroundColor = #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)
-        view.translatesAutoresizingMaskIntoConstraints = false
+        
         return view
     }()
     
     let topView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = #colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1)
+        return view
+    }()
+    
+    let postLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = Constants.postLabelFont
+        label.backgroundColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
+        label.textColor = #colorLiteral(red: 0.1725490196, green: 0.1764705882, blue: 0.1803921569, alpha: 1)
+        return label
+    }()
+    
+    let postImageView: WebImageView = {
+        let imageView = WebImageView()
+        imageView.backgroundColor = #colorLiteral(red: 0.8901960784, green: 0.8980392157, blue: 0.9098039216, alpha: 1)
+        return imageView
+    }()
+    
+    let bottomView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
         return view
     }()
     
@@ -30,13 +53,35 @@ final class NewsfeedCodeCell: UITableViewCell {
         backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
         
         overlayFirstLayer()
-        // MARK: - cardView constraints
-        cardView.anchor(top: topAnchor,
-                        leading: leadingAnchor,
-                        bottom: nil,
-                        trailing: nil,
-                        padding: UIEdgeInsets(top: 8, left: 8, bottom: 0, right: 0),
-                        size: CGSize(width: 40, height: 40))
+        overlaySecondLayer()
+    }
+    
+    func set(viewModel: FeedCellViewModel) {
+        postLabel.text = viewModel.text
+        
+        postLabel.frame = viewModel.sizes.postLabelFrame
+        postImageView.frame = viewModel.sizes.attachementFrame
+        bottomView.frame = viewModel.sizes.bottomViewFrame
+        
+        if let photoAttachement = viewModel.photoAttachement {
+            postImageView.set(imageURL: photoAttachement.photoUrlString)
+            postImageView.isHidden = false
+        } else {
+            postImageView.isHidden = true
+        }
+    }
+    
+    func overlaySecondLayer() {
+        cardView.addSubview(topView)
+        cardView.addSubview(postLabel)
+        cardView.addSubview(postImageView)
+        cardView.addSubview(bottomView)
+        
+        // topView constraints
+        topView.leadingAnchor.constraint(equalTo: cardView.leadingAnchor, constant: 8).isActive = true
+        topView.trailingAnchor.constraint(equalTo: cardView.trailingAnchor, constant: -8).isActive = true
+        topView.topAnchor.constraint(equalTo: cardView.topAnchor, constant: 8).isActive = true
+        topView.heightAnchor.constraint(equalToConstant: Constants.topViewHeight).isActive = true
     }
     
     func overlayFirstLayer() {
