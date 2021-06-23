a7f37a48e8478fc68ff2d59aa0111e70c7ce55b7
Al <alexunder_5542@mail.ru>
2021-06-24 00:12:15 +0300

    added   override var intrinsicContentSize: CGSize {         return UIView.layoutFittingExpandedSize     } into TitleView class

--- VKFeed/Newsfeed/TitleView/TitleView.swift
+++ VKFeed/Newsfeed/TitleView/TitleView.swift
@@ -9,11 +9,9 @@ import Foundation
 import UIKit
 
 class TitleView: UIView {
-//
-//    private var myTextField: UITextField = {
-//
-//    }()
-//
+
+    private var myTextField = InsetableTextField()
+
     private var myAvatarView: WebImageView = {
         let imageView = WebImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -24,6 +22,41 @@ class TitleView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        addSubview(myTextField)
+        addSubview(myAvatarView)
+    }
+    
+    private func makeConstraints() {
+        // myAvatarView constaints
+        myAvatarView.anchor(top: topAnchor,
+                            leading: nil,
+                            bottom: nil,
+                            trailing: trailingAnchor,
+                            padding: UIEdgeInsets(top: 4, left: 0, bottom: 0, right: 4))
+        
+        myAvatarView.heightAnchor.constraint(equalTo: myTextField.heightAnchor, multiplier: 1).isActive = true
+        myAvatarView.widthAnchor.constraint(equalTo: myTextField.widthAnchor, multiplier: 1).isActive = true
+        
+        
+        // myTextField constaints
+        myTextField.anchor(top: topAnchor,
+                           leading: leadingAnchor,
+                           bottom: bottomAnchor,
+                           trailing: myAvatarView.leadingAnchor,
+                           padding: UIEdgeInsets(top: 4, left: 4, bottom: 4, right: 12))
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return UIView.layoutFittingExpandedSize
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        myAvatarView.layer.masksToBounds = true
+        myAvatarView.layer.cornerRadius = myAvatarView.frame.width / 2
     }
     
     required init?(coder: NSCoder) {
