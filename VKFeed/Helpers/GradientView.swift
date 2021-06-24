2bd01fa3ffc0478cb997d3d73f1058786baac000
Al <alexunder_5542@mail.ru>
2021-06-24 14:50:24 +0300

    added gradientLayer

--- VKFeed/Helpers/GradientView.swift
+++ VKFeed/Helpers/GradientView.swift
@@ -9,13 +9,25 @@ import Foundation
 import UIKit
 
 class GradientView: UIView {
+    
+    private let gradientLayer = CAGradientLayer()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        backgroundColor = .blue
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    required init?(coder Decoder: NSCoder) {
+        super.init(coder: Decoder)
+        setupGradient()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = bounds
+    }
+    
+    private func setupGradient() {
+        self.layer.addSublayer(gradientLayer)
+        gradientLayer.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
     }
 }
